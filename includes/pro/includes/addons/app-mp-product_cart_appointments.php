98738ba637fe8afcbb71f24dc91184273b9145b1
Dernerd <webmaster@n3rds.work>
2024-02-27 22:14:10 +0100

    Links

--- includes/pro/includes/addons/app-mp-product_cart_appointments.php
+++ includes/pro/includes/addons/app-mp-product_cart_appointments.php
@@ -5,7 +5,7 @@ Description: Steuere, wie Deine Termine im Produktkorb angezeigt werden.
 Plugin URI: https://cp-psource.github.io/terminmanager-pro/
 Version: 1.0
 AddonType: Integration
-Requires: <a href="https://n3rds.work/piestingtal-source-project/psecommerce-plugin/">PSeCommerce</a>
+Requires: <a href="https://https://github.com/cp-psource/psecommerce/releases">PSeCommerce</a>
 Author: PSOURCE
 */
 
