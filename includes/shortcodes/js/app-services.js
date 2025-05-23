4b531367e6f0f3bfd50568c0ce59931c322a9415
DerN3rd <dern3rd@outlook.at>
2025-05-23 19:13:01 +0200

    Refactor app-services.js and my-appointments.js for improved readability and maintainability
    - Updated app-services.js to use modern JavaScript syntax and improved variable naming for clarity.
    - Removed source map file for app-services.js as it is no longer needed.
    - Refactored my-appointments.js to enhance structure and readability, including better handling of appointment cancellation and payment processing.
    - Deleted source map file for my-appointments.js to streamline the codebase.

--- includes/shortcodes/js/app-services.js
+++ includes/shortcodes/js/app-services.js
@@ -1,2 +1,56 @@
-!function(t){function e(i){if(n[i])return n[i].exports;var s=n[i]={i:i,l:!1,exports:{}};return t[i].call(s.exports,s,s.exports,e),s.l=!0,s.exports}var n={};e.m=t,e.c=n,e.i=function(t){return t},e.d=function(t,n,i){e.o(t,n)||Object.defineProperty(t,n,{configurable:!1,enumerable:!0,get:i})},e.n=function(t){var n=t&&t.__esModule?function(){return t.default}:function(){return t};return e.d(n,"a",n),n},e.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},e.p="",e(e.s=1)}([,function(t,e){jQuery(document).ready(function(t){"use strict";window.Appointments=window.Appointments||{},Appointments.shortcodes=Appointments.shortcodes||{},Appointments.shortcodes.services={thumbnailsCache:[],strings:null,init:function(){this.strings=appointmentsStrings,this.$servicesSelector=t(".app_select_services"),this.$submitButton=t(".app_services_button"),"1"==this.strings.autorefresh&&this.$submitButton.hide(),this.on()},bind:function(){var e=this;this.$submitButton.on('click', function(){var t=e.$servicesSelector.val();void 0!==t&&null!==t||(t=e.strings.first_service_id),e.reload(t)}),this.$servicesSelector.on('change',function(){var n=t(this).val();if("1"==e.strings.autorefresh)return e.$submitButton.trigger("click"),!1;t(".app_service_excerpt").hide(),t("#app_service_excerpt_"+n).show()})},reload:function(t){window.location.href=this.strings.reload_url.replace("__selected_service__",t)}},Appointments.shortcodes.services.init()})}]);
-//# sourceMappingURL=app-services.js.map
+jQuery(document).ready(function($) {
+    "use strict";
+
+    window.Appointments = window.Appointments || {};
+    Appointments.shortcodes = Appointments.shortcodes || {};
+
+    Appointments.shortcodes.services = {
+        thumbnailsCache: [],
+        strings: null,
+
+        init: function() {
+            var self = this;
+            this.strings = appointmentsStrings;
+            this.$servicesSelector = $('.app_select_services');
+            this.$submitButton = $('.app_services_button');
+
+            if ( this.strings.autorefresh == '1' ) {
+                this.$submitButton.hide();
+            }
+
+            this.bind();
+        },
+
+        bind: function() {
+            var self = this;
+
+            this.$submitButton.on("click", function(){
+                var selected_service = self.$servicesSelector.val();
+                if ( typeof selected_service=='undefined' || selected_service===null ) {
+                    selected_service=self.strings.first_service_id;
+                }
+
+                self.reload( selected_service );
+            });
+
+            this.$servicesSelector.on('change', function(){
+                var selected_service = $(this).val();
+
+                if ( self.strings.autorefresh == '1' ) {
+                    self.$submitButton.trigger( 'click' );
+                    return false;
+                }
+
+                $('.app_service_excerpt').hide();
+                $('#app_service_excerpt_'+selected_service).show();
+            });
+        },
+
+        reload: function( service ) {
+            window.location.href = this.strings.reload_url.replace('__selected_service__', service);
+        }
+
+    };
+
+    Appointments.shortcodes.services.init();
+});
--- includes/shortcodes/js/my-appointments.js
+++ includes/shortcodes/js/my-appointments.js
@@ -1,2 +1,104 @@
-!function(n){function t(a){if(e[a])return e[a].exports;var p=e[a]={i:a,l:!1,exports:{}};return n[a].call(p.exports,p,p.exports,t),p.l=!0,p.exports}var e={};t.m=n,t.c=e,t.i=function(n){return n},t.d=function(n,e,a){t.o(n,e)||Object.defineProperty(n,e,{configurable:!1,enumerable:!0,get:a})},t.n=function(n){var e=n&&n.__esModule?function(){return n.default}:function(){return n};return t.d(e,"a",e),e},t.o=function(n,t){return Object.prototype.hasOwnProperty.call(n,t)},t.p="",t(t.s=2)}({2:function(n,t){Appointments=window.Appointments||{},function(n,t,e){"use strict";function a(n){return e(".app-my-appointments-cancel").on('change',function(n){var a=e(n.target),p=this.cancelAppointment;a.is(":checked")&&swal({title:t.aysCancel,text:"",type:"warning",showCancelButton:!0,cancelButtonText:t.no,confirmButtonText:t.yes,closeOnConfirm:!0},function(n){n?p(a.data("app-id")):a.attr("checked",!1)})}.on(this)),this}a.prototype.cancelAppointment=function(n){var a={action:"cancel_user_app",app_id:n,cancel_nonce:t.nonce};e.post(t.ajaxurl,a,function(a){var p=e("input#cancel-"+n);if(void 0===a.success)return p.attr("disabled",!0),void swal(t.connectionError,"","error");a.success?(swal(t.cancelled,"","success"),p.closest("tr").css("opacity","0.3"),p.attr("disabled",!0)):swal(a.data)},"json")},n.myAppointments=function(n){return new a(n)},e("body").on("click",".appointments-paid-button",function(){var n=e(this).data("appointment");if(!(n&&n.ID&&n.price&&n.service_name))return!1;var t={price:n.price,app_id:n.ID,service_name:n.service_name};e(".appointments-paypal").find(".app_amount").val(t.price),e(".appointments-paypal").find(".app_custom").val(t.app_id);var a=e(".appointments-paypal").find(".app_submit_btn").val();if(a){var p=a.replace("PRICE",t.price).replace("SERVICE",t.service_name);e(".appointments-paypal").find(".app_submit_btn").val(p);var i=e(".appointments-paypal").find(".app_item_name").val(),c=i.replace("SERVICE",t.service_name);e(".appointments-paypal").find(".app_item_name").val(c),e(".appointments-paypal .app_submit_btn").focus()}e(".appointments-paypal").find("form").submit()})}(Appointments,appMyAppointmentsStrings,jQuery)}});
-//# sourceMappingURL=my-appointments.js.map
+Appointments = window.Appointments || {};
+
+( function( global, strings, $ ) {
+    "use strict";
+
+    /**
+     * Constructor
+     *
+     * @param options
+     * @returns {MyAppointments}
+     * @constructor
+     */
+    function MyAppointments( options ) {
+        $(".app-my-appointments-cancel").on('change',  function(e) {
+            var $target = $( e.target );
+            var cancelAppointment = this.cancelAppointment;
+            if ( $target.is( ':checked' ) ) {
+                swal({
+                        title: strings.aysCancel,
+                        text: "",
+                        type: "warning",
+                        showCancelButton: true,
+                        cancelButtonText: strings.no,
+                        confirmButtonText: strings.yes,
+                        closeOnConfirm: true
+                    },
+                    function( isConfirm ){
+                        if ( ! isConfirm ) {
+                            $target.attr("checked", false);
+                        }
+                        else {
+                            cancelAppointment( $target.data( 'app-id' ) );
+                        }
+                    });
+
+            }
+        }.bind( this ));
+        return this;
+    }
+
+    MyAppointments.prototype.cancelAppointment = function( appId ) {
+        var data = {
+            action: "cancel_user_app",
+            app_id: appId,
+            cancel_nonce: strings.nonce
+        };
+
+        $.post(strings.ajaxurl, data, function(response) {
+            var cancel_box = $('input#cancel-' + appId );
+            if ( typeof response.success === 'undefined' ) {
+                cancel_box.attr("disabled",true);
+                swal( strings.connectionError, '', 'error' );
+                return;
+            }
+
+            if ( response.success ) {
+                swal( strings.cancelled, '', 'success' );
+                cancel_box.closest("tr").css("opacity","0.3");
+                cancel_box.attr("disabled",true);
+            }
+            else {
+                swal(response.data);
+            }
+        }, "json");
+    };
+
+
+
+    /**
+     * Helper constructor function
+     *
+     * @param options
+     * @returns {MyAppointments}
+     */
+    global.myAppointments = function( options ) {
+        return new MyAppointments( options );
+    };
+
+
+	$('body').on( 'click', '.appointments-paid-button', function() {
+		var appointment_data = $(this).data('appointment');
+		if ( !appointment_data || !appointment_data.ID || !appointment_data.price || !appointment_data.service_name )
+			return false;
+		var post_data = {
+			price: appointment_data.price,
+			app_id: appointment_data.ID,
+			service_name: appointment_data.service_name
+		};
+		$(".appointments-paypal").find(".app_amount").val(post_data.price);
+		$(".appointments-paypal").find(".app_custom").val(post_data.app_id);
+		var old_val = $(".appointments-paypal").find(".app_submit_btn").val();
+		if ( old_val ) {
+			var new_val = old_val.replace("PRICE",post_data.price).replace("SERVICE",post_data.service_name);
+			$(".appointments-paypal").find(".app_submit_btn").val(new_val);
+			var old_val2 = $(".appointments-paypal").find(".app_item_name").val();
+			var new_val2 = old_val2.replace("SERVICE",post_data.service_name);
+			$(".appointments-paypal").find(".app_item_name").val(new_val2);
+			$(".appointments-paypal .app_submit_btn").focus();
+		}
+
+		$(".appointments-paypal").find("form").submit();
+	});
+
+})( Appointments, appMyAppointmentsStrings, jQuery );
